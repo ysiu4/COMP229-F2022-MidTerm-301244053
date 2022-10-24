4bba03cbe9cfe52fe6f31c1e93220c0b63cfb71b
Vic Siu <ysiu4@my.centennialcollege.ca>
2022-10-24 18:28:37 -0400

    update URI of mongo db atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  URI: "mongodb://127.0.0.1/car_store",
+  // URI: "mongodb://127.0.0.1/car_store",
+  URI: 'mongodb+srv://yukmingsiu:2tCNBZyv78rb6Wpt@myfirstcluster.gn8bio1.mongodb.net/car_store?retryWrites=true&w=majority'
 };
