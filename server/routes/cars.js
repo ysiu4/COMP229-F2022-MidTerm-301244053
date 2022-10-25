dee4224ae7f5deea7d9d1e204178881286c712bd
Vic Siu <ysiu4@my.centennialcollege.ca>
2022-10-25 12:57:44 -0400

    1. implemented "add" 2. implemented "edit"

--- server/routes/cars.js
+++ server/routes/cars.js
@@ -26,6 +26,10 @@ router.get("/add", (req, res, next) => {
   /*****************
    * ADD CODE HERE *
    *****************/
+  // DONE
+   res.render('cars/add', {
+    title: 'Add a Car',
+  });
 });
 
 // POST process the Car  Details page and create a new Car  - CREATE
@@ -33,6 +37,19 @@ router.post("/add", (req, res, next) => {
   /*****************
    * ADD CODE HERE *
    *****************/
+  // DONE
+  new_car_record = {
+    Carname: req.body.Carname,
+    Category: req.body.Category,
+    Carmodel: req.body.Carmodel,
+    Price: parseInt(req.body.Price),
+  };
+  car.create(new_car_record, (err, car_record) => {
+    if (err) {
+      console.log('Insert document error: ' + err);
+    }
+    res.redirect('/cars');
+  });
 });
 
 // GET the Car Details page in order to edit an existing Car
@@ -40,6 +57,19 @@ router.get("/:id", (req, res, next) => {
   /*****************
    * ADD CODE HERE *
    *****************/
+  // DONE
+  let id = req.params.id;
+  car.findById(id, (err, car_record) => {
+    if (err) {
+      console.log('Find document error: ' + err);
+      res.redirect('/cars');
+    } else {
+      res.render('cars/details', {
+        title: 'Edit a Car',
+        cars: car_record,
+      });
+    }
+  });
 });
 
 // POST - process the information passed from the details form and update the document
@@ -47,6 +77,20 @@ router.post("/:id", (req, res, next) => {
   /*****************
    * ADD CODE HERE *
    *****************/
+  // DONE
+   new_car_record = {
+    Carname: req.body.Carname,
+    Category: req.body.Category,
+    Carmodel: req.body.Carmodel,
+    Price: parseInt(req.body.Price),
+  };
+  let id = req.params.id;
+  car.updateOne({_id: id}, new_car_record, (err, car_record) => {
+    if (err) {
+      console.log('Update document error: ' + err);
+    }
+    res.redirect('/cars');
+  });
 });
 
 // GET - process the delete
